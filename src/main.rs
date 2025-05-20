74affe4af3292f5e2368d1b00f690f520f9eb73e
7000 <60473870+Garrett7k@users.noreply.github.com>
2025-05-20 16:47:28 -0400

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ impl EventHandler for Handler {
 //Current list of commands
 //When adding extra commands, you must add the command call to this list.
 #[group]
-#[commands(card_guild, card_dm, doublecard, blackwhitelands, randomsearch_guild, randomsearch_dm)]
+#[commands(card, doublecard, blackwhitelands, randomsearch)]
 struct General;
 
 #[tokio::main]
@@ -89,7 +89,7 @@ async fn main() {
 #[command]
 #[aliases(scry, s)]
 #[only_in(guilds)]
-async fn card_guild(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+async fn card(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     msg_clean_up(ctx, msg).await;
 
     let card_name = args.raw().collect::<Vec<&str>>().join(" ");
@@ -205,16 +205,6 @@ async fn doublecard(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
     Ok(())
 }
 
-async fn msg_clean_up(ctx: &Context, msg: &Message) {
-    msg.delete(&ctx.http).await.unwrap();
-}
-
-/// Checks that a message successfully sent; if not, then logs why to stdout.
-fn check_msg(result: SerenityResult<Message>) {
-    if let Err(why) = result {
-        println!("Error sending message: {:?}", why);
-    }
-}
 
 #[command]
 #[aliases(lbw, wbl)]
@@ -254,10 +244,12 @@ async fn blackwhitelands(ctx: &Context, msg: &Message) -> CommandResult {
 
     Ok(())
 }
+
+
 #[command]
 #[aliases(sr)]
 #[only_in(guilds)]
-async fn randomsearch_guild(ctx: &Context, msg: &Message) -> CommandResult {
+async fn randomsearch(ctx: &Context, msg: &Message) -> CommandResult {
    msg_clean_up(ctx, msg).await;
 
     let now = Instant::now();
@@ -313,115 +305,14 @@ async fn randomsearch_guild(ctx: &Context, msg: &Message) -> CommandResult {
 
 
 
-#[command]
-#[aliases(scrydm, sdm)]
-async fn card_dm(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
-   // msg_clean_up(ctx, msg).await;
 
-    let card_name = args.raw().collect::<Vec<&str>>().join(" ");
-    let now = Instant::now();
-
-    let card = match Card::named_fuzzy(card_name.as_str()).await {
-        Ok(card) => card,
-        Err(e) => panic!(
-            "{}",
-            format!(
-                "{:?}, {:?}",
-                e.to_string(),
-                check_msg(msg.channel_id.say(&ctx.http, e.to_string()).await),
-            )
-        ),
-    };
-
-    let card_legal_m = card.legalities.modern;
-    let card_legal_p = card.legalities.pauper;
-    let card_legal_c = card.legalities.commander;
-    let card_set = card.set_name;
-
-    let _card_price = match card.prices.usd {
-        Some(card_price) => format!("{:?}", check_msg(
-               msg.channel_id
-                    .say(&ctx.http, format!("Card Price (USD): ${card_price}"))
-                    .await,
-            )),
-        None => format!(
-            "{:?}",
-            check_msg(
-                msg.channel_id
-                    .say(&ctx.http, format!("Unable to find Card Price (USD)"))
-                    .await
-            )
-        ),
-    };
-
-    let card_image = match card.image_uris {
-    	Some(card_image) => card_image.png,
-    	None => panic!("{}", format!(
-                "{:?}, {:?}",
-                check_msg(msg.channel_id.say(&ctx.http, format!("Unable to find Card Image (Possibly a double faced card). Here is Scryfall link:")).await),
-       			 check_msg(msg.channel_id.say(&ctx.http, card.scryfall_uri).await)
-            ))
-    };
-
-    check_msg(msg.channel_id.say(&ctx.http, card_image.unwrap()).await);
-    let timeframe = now.elapsed().as_millis();
-    check_msg(msg.channel_id.say(&ctx.http, format!("Fetch time in ms: {timeframe}\nModern: {card_legal_m:?}\nPauper: {card_legal_p:?}\nCommander: {card_legal_c:?}\nPrinting: {card_set}")).await);
-
-    Ok(())
+async fn msg_clean_up(ctx: &Context, msg: &Message) {
+    msg.delete(&ctx.http).await.unwrap();
 }
 
-#[command]
-#[aliases(srdm)]
-async fn randomsearch_dm(ctx: &Context, msg: &Message) -> CommandResult {
-   //msg_clean_up(ctx, msg).await;
-
-    let now = Instant::now();
-    let card = match Card::random().await {
-        Ok(card) => card,
-        Err(e) => panic!(
-            "{}",
-            format!(
-                "{:?}",
-                check_msg(msg.channel_id.say(&ctx.http, e.to_string()).await)
-            )
-        ),
-    };
-
-    let card_legal_m = card.legalities.modern;
-    let card_legal_p = card.legalities.pauper;
-    let card_legal_c = card.legalities.commander;
-    let card_set = card.set_name;
-
-    
-	 let _card_price = match card.prices.usd {
-        Some(card_price) => format!("{:?}", check_msg(
-               msg.channel_id
-                    .say(&ctx.http, format!("Random ass card found! (USD): ${card_price}"))
-                    .await,
-            )),
-        None => format!(
-            "{:?}",
-            check_msg(
-                msg.channel_id
-                    .say(&ctx.http, format!("Unable to find Card Price (USD)"))
-                    .await
-            )
-        ),
-    };
-
-    let card_image = match card.image_uris {
-    	Some(card_image) => card_image.png,
-    	None => panic!("{}", format!(
-                "{:?}, {:?}",
-                check_msg(msg.channel_id.say(&ctx.http, format!("Unable to find Card Image (Possibly a double faced card). Here is Scryfall link:")).await),
-       			 check_msg(msg.channel_id.say(&ctx.http, card.scryfall_uri).await)
-            ))
-    };
-
-    check_msg(msg.channel_id.say(&ctx.http, card_image.unwrap()).await);
-    let timeframe = now.elapsed().as_millis();
-    check_msg(msg.channel_id.say(&ctx.http, format!("Fetch time in ms: {timeframe}\nModern: {card_legal_m:?}\nPauper: {card_legal_p:?}\nCommander: {card_legal_c:?}\nPrinting: {card_set}")).await);
-
-
-    Ok(())
+/// Checks that a message successfully sent; if not, then logs why to stdout.
+fn check_msg(result: SerenityResult<Message>) {
+    if let Err(why) = result {
+        println!("Error sending message: {:?}", why);
+    }
 }
